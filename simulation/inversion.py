a093e01344f52a797385b1a0a2f184c6e70c6ff5
Omer Sipra <omaruzairsipra68@gmail.com>
2026-05-01 21:14:40 -0400

    Add Tikhonov inversion (WIP - conditioning issues to be resolved)

--- simulation/inversion.py
+++ simulation/inversion.py
@@ -25,3 +25,21 @@ def alvarez_suter(t, n_list, omega, S_omega):
         S_reconstructed.append(S_n)
     
     return np.array(omega_probe), np.array(S_reconstructed)
+
+
+def tikhonov(t_list, chi_list, omega_recon, lambda_reg): #In-Progress
+    d_omega = np.diff(omega_recon, prepend=omega_recon[0])
+    
+    K = np.zeros((len(t_list), len(omega_recon)))
+    for i, t in enumerate(t_list):
+        n = max(1, i // 10 + 1)
+        K[i, :] = F_CPMG(omega_recon, t, n) * d_omega / (2 * np.pi)
+    
+    chi_vec = np.array(chi_list)
+    S = np.linalg.solve(
+        K.T @ K + lambda_reg * np.eye(len(omega_recon)),
+        K.T @ chi_vec
+    )
+    S = np.clip(S, 0, None)
+    return omega_recon, S
+
